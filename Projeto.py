0baee62fa75278f417ecb5af67ae8a40ddcdb4f6
J27mendez <mendes_j27@hotmail.com>
2025-09-03 20:46:48 -0300

    listar alunos e garantia que o usuario escreva a nota correta

--- Projeto.py
+++ Projeto.py
@@ -5,17 +5,35 @@ alunos= []
 def cadastrar_aluno():
     nome = input("Digite o nome do aluno: ")
     idade = int(input("Digite a idade do aluno: "))
-    nota = float(input("Digite a nota do aluno (0 a 10): "))
+
+    while True:
+        try:
+            nota = float(input("Digite a nota do aluno (0 a 10): "))
+            if 0 <= nota <= 10:
+                nota = round(nota, 1)  
+                break  
+            else:
+                print("A nota deve estar entre 0 e 10.")
+        except ValueError:
+            print("Por favor, insira um valor válido para a nota.")
 
     aluno = {
         "nome": nome,
         "idade": idade,
-        "nota": round(nota, 1)  # garante 1 casa decimal
+        "nota": nota,  
     }
 
     alunos.append(aluno)
     print(f"Aluno {nome} cadastrado com sucesso!\n")
 
+def listar_alunos():
+    if not alunos:
+        print("Nenhum aluno cadastrado. Cadastre primeiro!\n")
+        return
+    print("\n--- Lista de Alunos ---")
+    for i, aluno in enumerate(alunos, start=1):
+        print(f"{i}. Nome: {aluno['nome']} --- Idade: {aluno['idade']} --- Nota: {aluno['nota']}")
+    print()
 
 while True:
     print("*****===== MENU =====*****")
@@ -28,7 +46,7 @@ while True:
     opcao = input("Escolha uma opção: ")
 
     if not opcao.isdigit():  # garante que é um número
-        print("⚠ Digite apenas números de 1 a 5.\n")
+        print("Digite apenas números de 1 a 5.\n")
         continue
 
     opcao = int(opcao)
@@ -36,7 +54,7 @@ while True:
     if opcao == 1:
         cadastrar_aluno()
     elif opcao == 2:
-        print("listar_alunos()")
+        listar_alunos()
     elif opcao == 3:
         print("buscar_aluno()")
     elif opcao == 4:
