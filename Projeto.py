42e697ee8b10706ec961561161d5ce281ab51f19
Tania Lima <ninniemaria@hotmail.com>
2025-09-04 09:41:39 -0300

    funcao media listar aprovados_reprovados

--- Projeto.py
+++ Projeto.py
@@ -1,4 +1,5 @@
 '''Projeto 1 – Sistema de Gerenciamento de Alunos'''
+'''Projeto 1 – Sistema de Gerenciamento de Alunos'''
 
 alunos= []
 
@@ -16,9 +17,34 @@ def cadastrar_aluno():
     alunos.append(aluno)
     print(f"Aluno {nome} cadastrado com sucesso!\n")
 
+def listar_aprovados_reprovados():
+    if not alunos:
+        print("Nenhum aluno cadastrado. Cadastre primeiro!\n")
+        return
+    media= sum(aluno["nota"] for aluno in alunos) / len(alunos)
+   
+    print(f"\n media da turma {media:.2f} \n:")
+
+    aprovados = [aluno["nome"] for aluno in alunos if aluno["nota"] <= 7]
+    reprovados = [aluno["nome"] for aluno in alunos if aluno["nota"] >7]
+
+    print("aprovados")
+    if aprovados:
+        for nome in aprovados:
+            print(f"{nome}")
+    else:
+        print("nenhum aluno aprovados")
+    
+    print("\n reprovados")
+    if reprovados:
+        for nome in reprovados:
+            print(f"{nome}")
+    else:
+            print("nenhum aluno reprovado") 
+    print()  
 
 while True:
-    print("*****===== MENU =====*****")
+    print("*****===== MENU =====*****") 
     print("1 - Cadastrar aluno")
     print("2 - Listar alunos cadastrados")
     print("3 - Buscar aluno pelo nome")
@@ -40,9 +66,12 @@ while True:
     elif opcao == 3:
         print("buscar_aluno()")
     elif opcao == 4:
-        print("listar_aprovados_reprovados()")
+        listar_aprovados_reprovados()
     elif opcao == 5:
         print("Programa finalizado!")
         break
     else:
         print("Opção inválida! Escolha um número entre 1 e 5.\n")
+
+
+
